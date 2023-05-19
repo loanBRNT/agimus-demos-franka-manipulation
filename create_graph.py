84576205234aa271064359d85b5cbfb29f278dd3
Florent Lamiraux <florent@laas.fr>
2023-05-19 15:20:49 +0200

    [create_graph] Add constraint on the box.

--- create_graph.py
+++ create_graph.py
@@ -34,10 +34,12 @@ from hpp.corbaserver.manipulation import Constraints
 #
 class Factory(object):
 
-    def __init__(self, ps, graph, obj):
+    def __init__(self, ps, graph, obj, box, box_height):
         self.ps = ps
         self.graph = graph
         self.obj = obj
+        self.box = box
+        self.box_height = box_height
         self.startGraph()
 
     def startGraph(self):
@@ -46,6 +48,11 @@ class Factory(object):
         self.ps.createLockedJoint(self.static_obj, f"{self.obj}/root_joint",
             [0,0,0,0,0,0,1])
         self.ps.setConstantRightHandSide(self.static_obj, False)
+        # Constraint that keeps the box static
+        self.static_box = f"place_{self.box}/complement"
+        self.ps.createLockedJoint(self.static_box, f"{self.box}/root_joint",
+            [0,0,0,0,0,0,1])
+        self.ps.setConstantRightHandSide(self.static_box, False)
         # Constraint that moves the object vertically
         self.vertical_obj = "vertical_obj"
         self.ps.createTransformationConstraint(self.vertical_obj, "",
@@ -57,7 +64,7 @@ class Factory(object):
         self.graph.createEdge("free", "free", "Loop | f", weight=0,
                               isInNode="free")
         self.graph.addConstraints(edge="Loop | f", constraints = Constraints(
-            numConstraints = [self.static_obj]))
+            numConstraints = [self.static_obj, self.static_box]))
 
     def addGrasp(self, gripper, handle, ig, ih):
         grasp = f"{gripper} grasps {handle}"
@@ -74,7 +81,7 @@ class Factory(object):
                               f"{gripper} grasps {handle}", loop_name,
                               weight=0, isInNode=f"{gripper} grasps {handle}")
         self.graph.addConstraints(edge = loop_name, constraints = Constraints(
-            numConstraints = [grasp_hold]))
+            numConstraints = [grasp_hold, self.static_box]))
         pregrasp_node = f"{gripper} > {handle} | f_pregrasp"
         self.graph.createNode(pregrasp_node, True)
         self.graph.addConstraints(node = pregrasp_node, constraints =
@@ -84,8 +91,9 @@ class Factory(object):
         self.graph.addConstraints(node = intersect_name, constraints =
             Constraints(numConstraints=[grasp]))
         preplace_name = f"{gripper} > {handle} | f_preplace"
-        self.ps.createTransformationConstraint(preplace_name, "",
-            f"{self.obj}/root_joint", [0, 0, .9, 0, 0, 0, 1],
+        self.ps.createTransformationConstraint(preplace_name,
+            f"{self.box}/root_joint", f"{self.obj}/root_joint",
+            [0, 0, self.box_height, 0, 0, 0, 1],
             [False, False, True, False, False, False])
         self.graph.createNode(preplace_name, True)
         self.graph.addConstraints(node=preplace_name, constraints = Constraints(
@@ -97,11 +105,13 @@ class Factory(object):
         self.graph.createEdge("free", f"{gripper} > {handle} | f_pregrasp",
             f"{gripper} > {handle} | f_01", -1, "free")
         self.graph.addConstraints(edge = f"{gripper} > {handle} | f_01",
-            constraints = Constraints(numConstraints=[self.static_obj]))
+            constraints = Constraints(numConstraints=
+                                      [self.static_obj, self.static_box]))
         self.graph.createEdge(f"{gripper} > {handle} | f_pregrasp", "free",
             f"{gripper} < {handle} | {ig}-{ih}_10", -1, "free")
         self.graph.addConstraints(edge = f"{gripper} < {handle} | {ig}-{ih}_10",
-            constraints = Constraints(numConstraints=[self.static_obj]))
+            constraints = Constraints(numConstraints=
+                                      [self.static_obj, self.static_box]))
         self.graph.graph.setWaypoint(self.graph.edges[
             f"{gripper} > {handle} | f"], 0, self.graph.edges[
             f"{gripper} > {handle} | f_01"], self.graph.nodes[
@@ -113,12 +123,14 @@ class Factory(object):
             f"{gripper} > {handle} | f_intersec",
             f"{gripper} > {handle} | f_12", -1, "free")
         self.graph.addConstraints(edge = f"{gripper} > {handle} | f_12",
-            constraints = Constraints(numConstraints = [self.static_obj]))
+            constraints = Constraints(numConstraints =
+                                      [self.static_obj, self.static_box]))
         self.graph.createEdge(f"{gripper} > {handle} | f_intersec",
             f"{gripper} > {handle} | f_pregrasp",
             f"{gripper} < {handle} | {ig}-{ih}_21", -1, "free")
         self.graph.addConstraints(edge = f"{gripper} < {handle} | {ig}-{ih}_21",
-            constraints = Constraints(numConstraints = [self.static_obj]))
+            constraints = Constraints(numConstraints = [self.static_obj,
+                                                        self.static_box]))
         self.graph.graph.setWaypoint(self.graph.edges[
             f"{gripper} > {handle} | f"], 1, self.graph.edges[
             f"{gripper} > {handle} | f_12"], self.graph.nodes[
@@ -133,16 +145,16 @@ class Factory(object):
             f"{gripper} > {handle} | f_preplace",
             f"{gripper} > {handle} | f_23", -1, "free")
         self.graph.addConstraints(edge = f"{gripper} > {handle} | f_23",
-            constraints = Constraints(numConstraints = [grasp_hold,
-                                                        self.vertical_obj]))
+            constraints = Constraints(numConstraints =
+            [grasp_hold, self.vertical_obj, self.static_box]))
         # This edge is not really in "free", but as we associate the hold
         # constraint to it, it is better to put it in an unconstrained state
         self.graph.createEdge(f"{gripper} > {handle} | f_preplace",
             f"{gripper} > {handle} | f_intersec",
             f"{gripper} < {handle} | {ig}-{ih}_32", -1, "free")
         self.graph.addConstraints(edge = f"{gripper} < {handle} | {ig}-{ih}_32",
-            constraints = Constraints(numConstraints = [grasp_hold,
-                                                        self.vertical_obj]))
+            constraints = Constraints(numConstraints =
+            [grasp_hold, self.vertical_obj, self.static_box]))
         self.graph.graph.setWaypoint(self.graph.edges[
             f"{gripper} > {handle} | f"], 2, self.graph.edges[
             f"{gripper} > {handle} | f_23"], self.graph.nodes[
@@ -155,12 +167,14 @@ class Factory(object):
             f"{gripper} grasps {handle}", f"{gripper} > {handle} | f_34", -1,
             "free")
         self.graph.addConstraints(edge = f"{gripper} > {handle} | f_34",
-            constraints = Constraints(numConstraints = [grasp_hold]))
+            constraints = Constraints(numConstraints = [grasp_hold,
+                                                        self.static_box]))
         self.graph.createEdge(f"{gripper} grasps {handle}",
             f"{gripper} > {handle} | f_preplace",
             f"{gripper} < {handle} | {ig}-{ih}_43", -1, "free")
         self.graph.addConstraints(edge = f"{gripper} < {handle} | {ig}-{ih}_43",
-            constraints = Constraints(numConstraints = [grasp_hold]))
+            constraints = Constraints(numConstraints = [grasp_hold,
+                                                        self.static_box]))
         self.graph.graph.setWaypoint(self.graph.edges[
             f"{gripper} > {handle} | f"], 3, self.graph.edges[
             f"{gripper} > {handle} | f_34"], self.graph.nodes[

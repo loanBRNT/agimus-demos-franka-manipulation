e8db3bc9f11937f3ae3df45af390dd5f98c6fa6c
Florent Lamiraux <florent@laas.fr>
2023-05-19 15:21:35 +0200

    [script_hpp] Use ConstraintGraphFactory.

--- script_hpp.py
+++ script_hpp.py
@@ -34,7 +34,8 @@ from agimus_demos.tools_hpp import RosInterface
 from hpp.corbaserver import wrap_delete
 from create_graph import Factory
 from t_less import TLess
-from bin_picking import BinPicking
+from hpp.corbaserver.bin_picking import Client as BpClient
+from tools_hpp import displayHandle, generateTargetConfig, shootPartInBox
 
 connectedToRos = False
 
@@ -56,7 +57,8 @@ class Box:
     rootJointType = "freeflyer"
 
 defaultContext = "corbaserver"
-loadServerPlugin (defaultContext, "manipulation-corba.so")
+loadServerPlugin(defaultContext, "manipulation-corba.so")
+loadServerPlugin(defaultContext, "bin_picking.so")
 newProblem()
 
 robot = Robot("robot", "pandas", rootJointType="anchor")
@@ -83,15 +85,56 @@ print("Part and box loaded")
 robot.client.manipulation.robot.insertRobotSRDFModel\
     ("pandas", "package://agimus_demos/franka/manipulation/srdf/demo.srdf")
 
+# Discretize handles
+handles = list()
+nh = 16
+bpc = BpClient()
+
+for h in ['part/lateral_top', 'part/lateral_bottom', 'part/top',
+          'part/bottom' ]:
+    bpc.bin_picking.discretizeHandle(h, nh)
+    handles += ['%s_%03d' % (h, i) for i in range(nh)]
+
+# Create a handle and a gripper for the goal position of the object
+# Create specific graph with vertical preplace motions
 graph = ConstraintGraph(robot, 'graph')
-factory = Factory(ps, graph, "part")
-factory.addGrasp('pandas/panda2_gripper', 'part/lateral_top', 0, 0)
-factory.addGrasp('pandas/panda2_gripper', 'part/lateral_bottom', 0, 0)
-factory.addGrasp('pandas/panda2_gripper', 'part/top', 0, 1)
-factory.addGrasp('pandas/panda2_gripper', 'part/bottom', 0, 1)
+# create preplacement and placement constraints for the object
+ps.createTransformationConstraint("place_part", "", "part/root_joint",
+    [0,0,0,0,0,0,1], 6*[False])
+ps.createLockedJoint("place_part/complement", "part/root_joint",
+    [0,0,0,0,0,0,1])
+ps.setConstantRightHandSide("place_part/complement", False)
+ps.createTransformationConstraint("preplace_part", "box/root_joint",
+    "part/root_joint", [0, 0, .30, 0, 0, 0, 1],
+    [False, False, True, False, False, False])
+ps.createLockedJoint("place_box/complement", "box/root_joint",
+    [0,0,0,0,0,0,1])
+ps.setConstantRightHandSide("place_box/complement", False)
+ps.createTransformationConstraint("vertical_part", "",
+            f"part/root_joint", [0,0,0,0,0,0,1],
+            [True, True, False, True, True, True])
+ps.setConstantRightHandSide("vertical_part", False)
+
+factory = ConstraintGraphFactory(graph)
+factory.constraints.removeEmptyConstraints = False
+factory.setGrippers(['pandas/panda2_gripper'])
+factory.setObjects(["part"], [handles], [[]])
+factory.generate()
+for edge in graph.edges.keys():
+    graph.addConstraints(edge = edge,
+        constraints = Constraints(numConstraints = ["place_box/complement"]))
+gripper = "pandas/panda2_gripper"; ig = 0
+for ih, h in enumerate(handles):
+    edge = f"{gripper} > {h} | f_23"
+    graph.addConstraints(edge = edge,
+        constraints = Constraints(numConstraints = ["vertical_part"]))
+    edge = f"{gripper} < {h} | {ig}-{ih}_32"
+    graph.addConstraints(edge = edge,
+        constraints = Constraints(numConstraints = ["vertical_part"]))
+
 q0 = [0, -pi/4, 0, -3*pi/4, 0, pi/2, pi/4, 0.035, 0.035,
       0, 0, 1.2, 0, 0, 0, 1,
-      0, 0, 0.76, 0, 0, 0, 1]
+      0, 0, 0.761, 0, 0, 0, 1]
 # Lock gripper in open position.
 ps.createLockedJoint('locked_finger_1', 'pandas/panda2_finger_joint1', [0.035])
 ps.createLockedJoint('locked_finger_2', 'pandas/panda2_finger_joint2', [0.035])
@@ -109,8 +152,35 @@ if connectedToRos:
 else:
     q = q0[:]
 
-bp = BinPicking(ps, graph)
-configs = bp.generatePregrasps(q0, 'pandas/panda2_gripper', 'part/lateral_top', 5)
-configs = bp.generatePregrasps(q0, 'pandas/panda2_gripper', 'part/lateral_bottom', 5)
-configs += bp.generatePregrasps(q0, 'pandas/panda2_gripper', 'part/top', 5)
-configs += bp.generatePregrasps(q0, 'pandas/panda2_gripper', 'part/bottom', 5)
+# Create effector
+edge = 'pandas/panda2_gripper > part/lateral_top_000 | f_01'
+bpc.bin_picking.createEffector('effector', 'pandas/panda2_gripper', q0,
+                             graph.edges[edge])
+for i in range(5):
+    bpc.bin_picking.addObstacleToEffector('effector', f'box/base_link_{i}', 0.)
+
+found = False
+while not found:
+    q = shootPartInBox(robot, q0)
+    found, msg = robot.isConfigValid(q)
+
+freeHandles = list()
+
+for handle in handles:
+    res, msg = bpc.bin_picking.collisionTest('effector', handle, q)
+    if not res:
+        freeHandles.append(handle)
+
+freeGrasps = list()
+
+for handle in freeHandles:
+    edge = f"pandas/panda2_gripper > {handle} | f_01"
+    res, q1 = generateTargetConfig(robot, graph, edge, q)
+    if not res: continue
+    edge = f"pandas/panda2_gripper > {handle} | f_12"
+    res, q2 = generateTargetConfig(robot, graph, edge, q1)
+    if not res: continue
+    edge = f"pandas/panda2_gripper > {handle} | f_23"
+    res, q3 = generateTargetConfig(robot, graph, edge, q2)
+    if not res: continue
+    freeGrasps.append((q1, q2, q3))
